8c4038894fdae1c317eb78526bf401560eea7e9f
Edgar Marko Trono <marko@spacetime-eng.com>
2022-11-11 11:37:09 +0900

    #2 Added React project

---
+++ documentation/study/react-study-app/src/js/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+	return (
+		<div>
+			<h1>Hello World React</h1>
+			<h1>Hello World React Again</h1>
+			<ExpenseItem></ExpenseItem>
+		</div>
+	);
+}
+
+export default App;
